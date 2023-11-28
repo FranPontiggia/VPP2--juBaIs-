cef62bd6c622c5257452a79da3851d46b5d2a11d
Francisca <franpontiggia@gmail.com>
2023-11-28 16:58:19 -0300

    completar mas cards, formulario de contacto.

--- js/main.js
+++ js/main.js
@@ -20,4 +20,44 @@
     });
   });
 
-    
+  // Inicializa EmailJS
+  (function() {
+    // MI clave pública de EmailJS
+    emailjs.init('NFZ0f8AnPcq8mlMM-');
+  })();
+
+  window.onload = function() {
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+      event.preventDefault();
+      const btn = document.querySelector('button[type="submit"]');
+      btn.value = 'Enviando...';
+  
+      const serviceID = 'service_ndf8bew';
+      const templateID = 'template_ea6raia';
+  
+      emailjs.sendForm(serviceID, templateID, this)
+        .then(() => {
+          btn.value = 'ENVIAR MENSAJE';
+          document.getElementById('success-message').classList.remove('d-none');
+        document.getElementById('error-message').classList.add('d-none'); // Oculta el mensaje de error si estaba visible.
+          
+          // Restablecer los valores de los campos del formulario
+          document.getElementById('user_name').value = '';
+          document.getElementById('user_email').value = '';
+          document.getElementById('message').value = '';
+        })
+        .catch((err) => {
+          btn.value = 'ENVIAR MENSAJE';
+          document.getElementById('success-message').classList.add('d-none'); // Oculta el mensaje de éxito si estaba visible.
+          document.getElementById('error-message').classList.remove('d-none');
+          alert(JSON.stringify(err));
+        });
+    });
+  };
+  
+  // Manejar el envío del formulario de filtro
+const filterForm = document.getElementById("filter-form");
+filterForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  filterProperties();
+});
